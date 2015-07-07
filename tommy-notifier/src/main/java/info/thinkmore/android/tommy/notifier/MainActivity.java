d3ed2372acc95c582b746a90f591668acafa6749
Yang Chen <mike.c.yang@gmail.com>
2015-07-07 18:20:02 +0800

    Send first notification

--- tommy-notifier/src/main/java/info/thinkmore/android/tommy/notifier/MainActivity.java
+++ tommy-notifier/src/main/java/info/thinkmore/android/tommy/notifier/MainActivity.java
@@ -1,13 +1,22 @@
 package info.thinkmore.android.tommy.notifier;
 
+import lombok.val;
+
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
+import android.view.View;
+
 import org.androidannotations.annotations.*;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity
     extends Activity
 {
+
+    @SystemService
+    NotificationManager nm;
 
     @AfterViews
     void afterViews() {
@@ -19,4 +28,10 @@ public class MainActivity
         return true;
     }
 
+    @Click(R.id.btnNotify)
+    public void btnNotifyClicked(View clickedView){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("Test annotation").setContentText("Content info").setSmallIcon( R.drawable.ic_action_search );
+        nm.notify( 12, builder.build() );
+    }  
 }
